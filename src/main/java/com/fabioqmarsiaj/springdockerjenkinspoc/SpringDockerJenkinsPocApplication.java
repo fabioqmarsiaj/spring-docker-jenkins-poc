53e6f54a5b8990c0baaa1cce5a4fd41580e647c6
Fabio Marsiaj <fabiobackend@gmail.com>
2023-12-15 18:17:42 -0300

    first commit

---
+++ src/main/java/com/fabioqmarsiaj/springdockerjenkinspoc/SpringDockerJenkinsPocApplication.java
@@ -0,0 +1,13 @@
+package com.fabioqmarsiaj.springdockerjenkinspoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerJenkinsPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerJenkinsPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabioqmarsiaj/springdockerjenkinspoc/SpringDockerJenkinsPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabioqmarsiaj.springdockerjenkinspoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerJenkinsPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
